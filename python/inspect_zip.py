2dfbfc55158f1b51445dfc112f94fde35834a4e8
FurWil <wilsonfurman@outlook.com>
2026-08-22 01:59:19 +0000

    new path

--- python/inspect_zip.py
+++ python/inspect_zip.py
@@ -1,12 +1,12 @@
 import zipfile
 
 
-zip_file = "data/raw/FFIEC CDR Call Bulk XBRL 03312024.zip"
+zip_file = "data/raw/FFIEC CDR Call Bulk All Schedules 03312024.zip"
 
 
 with zipfile.ZipFile(zip_file, "r") as zip_ref:
 
-    print("Files inside the ZIP:")
+    print("Files inside the ZIP:\n")
 
     for file in zip_ref.namelist():
         print(file)
