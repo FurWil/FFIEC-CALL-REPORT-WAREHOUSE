5351bdd12b47851a85155ff75a618a4bf04285fe
FurWil <wilsonfurman@outlook.com>
2026-08-21 21:31:18 -0400

    Add functionality to inspect ZIP file contents

--- python/inspect_zip.py
+++ python/inspect_zip.py
@@ -1,1 +1,12 @@
+import zipfile
 
+
+zip_file = "data/raw/FFIEC CDR Call Bulk XBRL 03312024.zip"
+
+
+with zipfile.ZipFile(zip_file, "r") as zip_ref:
+
+    print("Files inside the ZIP:")
+
+    for file in zip_ref.namelist():
+        print(file)
