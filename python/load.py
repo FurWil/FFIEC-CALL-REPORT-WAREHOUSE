9f27a5c22c6f600eb59e3f02480810c5c7408778
FurWil <wilsonfurman@outlook.com>
2026-08-22 02:34:17 +0000

    create .env file

--- python/load.py
+++ python/load.py
@@ -1,1 +1,15 @@
+import psycopg
+
+
+connection = psycopg.connect(
+    host="localhost",
+    port=5432,
+    dbname="call_reports",
+    user="furwil",
+    password="1234"
+)
+
+print("Successfully connected to PostgreSQL!")
+
+connection.close()
 
